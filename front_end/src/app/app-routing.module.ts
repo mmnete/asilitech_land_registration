811577c17b21b7bebb8cfcf8a035379f8e04ae9d
Abdulrahman Salim <abdulrahman.salim@ucdconnect.ie>
2023-07-21 16:44:06 +0100

    initial log in ui set up

--- front_end/src/app/app-routing.module.ts
+++ front_end/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponentComponent } from './log-in-component/log-in-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LogInComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front_end/src/app/app.module.ts
+++ front_end/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogInComponentComponent } from './log-in-component/log-in-component.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppMaterialModule } from './app.material.module';
+
+const routes: Routes = [
+  { path: 'login', component: LogInComponentComponent }
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, RouterModule.forRoot(routes), ReactiveFormsModule, AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
